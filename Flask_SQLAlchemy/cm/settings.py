f8fd64bceb16dc898f89010753044b54f12ec5d1
digmyth <wxqcto@163.com>
2018-05-22 20:40:28 +0800

    Update settings.py

--- Flask_SQLAlchemy/cm/settings.py
+++ Flask_SQLAlchemy/cm/settings.py
@@ -1,6 +1,6 @@
 class Base():
     SECRET_KEY = 'sdfsdf'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:qwer1011@192.168.1.8:3306/db1?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:qoioy@192.168.1.8:3306/db1?charset=utf8'
     SQLALCHEMY_POOL_SIZE = 10
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
